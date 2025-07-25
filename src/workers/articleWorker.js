6c74c0786ed118b4c91baaff5e1e7790a7c07557
Widodo <widodo.rahadi00@gmail.com>
2025-07-25 17:46:31 +0700

    add: article_page_worker

---
+++ src/workers/articleWorker.js
@@ -0,0 +1,14 @@
+self.onmessage = function (e) {
+    const { articles, page, perPage } = e.data;
+
+    const sorted = [...articles].sort((a, b) => new Date(b?.created_at) - new Date(a?.created_at));
+    const start = (page - 1) * perPage;
+    const paginated = sorted.slice(start, start + perPage);
+
+
+    self.postMessage({
+        type: 'result',
+        paginated,
+        total: sorted.length,
+    });
+};
