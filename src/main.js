190b57c4f08edc6ea261efae4ff4d6938ce9c080
Widodo <widodo.rahadi00@gmail.com>
2025-09-12 23:31:10 +0700

    [local] commit

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import VueCookies from 'vue-cookies';
 import global from './appcore/global.js';
 import globalTest from './appcore/globalTest.js';
 import AppAlert from './components/BaseAlert.vue';
+import LoadScript from 'vue-plugin-load-script'
 import '@mdi/font/css/materialdesignicons.css'
 import '@fontsource/dm-sans';
 import '@fontsource/dm-sans/400.css';
@@ -18,51 +19,8 @@ import { AUTH_TOKEN, AUTH_USER, AUTH_GET_USER } from './stores/actions/reqApi.js
 const app = createApp(App);
 app.use(VueCookies);
 app.use(global);
+app.use(LoadScript);
 app.mixin(globalTest);
 app.component('AppAlert', AppAlert );
 
-// ---------- dapi init ---------------
-
-window.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const curdapi2 = new dapi2();
-        await curdapi2.init({
-            APP_REDIRECT_SSO: import.meta.env.VITE_APP_REDIRECT_SSO,
-            APP_CLIENT_ID: import.meta.env.VITE_APP_CLIENT_ID,
-            redirect_sso: import.meta.env.VITE_APP_REDIRECT_SSO_URL
-        });
-    
-        const authResult = await curdapi2.getAuth();
-        await store.dispatch(`auth/${AUTH_TOKEN}`, { ...authResult, thirdParty: curdapi2 });
-        const userProfile = await store.dispatch(`auth/${AUTH_GET_USER}`);
-        await store.dispatch(`auth/${AUTH_USER}`, userProfile)
-
-        app.config.globalProperties.$curdapi2 = curdapi2;
-    } catch (e) {
-        console.error(e);
-        // if (e?.status === 401) {
-        //     try {
-        //         await curdapi2.authLogout();
-        //     } catch (_) {}
-        //     location.reload();
-        // }
-    }
-});
-
-app.use(router);
-app.use(store);
-app.use(vuetify);
-app.mount('#app');
-
-// ------ by pass -----------
-
-// window.addEventListener("DOMContentLoaded", async() =>{ 
-//     try {
-//         app.use(router);
-//         app.use(store);
-//         app.use(vuetify);
-//         app.mount('#app');
-//     } catch (error) {
-//         console.error(error)
-//     }
-// });
+app.use(router).use(store).use(vuetify).mount('#app');
--- src/views/App.vue
+++ src/views/App.vue
@@ -32,6 +32,7 @@ import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import BaseIndex from './BaseIndex.vue';
 import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+import { AUTH_TOKEN, AUTH_USER, AUTH_GET_USER } from '../stores/actions/reqApi'
 
 export default {
     name: 'App',
@@ -50,13 +51,37 @@ export default {
     },
     methods: {
         ...mapActions({
-
         }),
+        async initDapi2() { 
+            window.addEventListener("DOMContentLoaded", async () => {
+                try {
+                    const curdapi2 = new dapi2();
+                    await curdapi2.init({
+                        APP_REDIRECT_SSO: import.meta.env.VITE_APP_REDIRECT_SSO,
+                        APP_CLIENT_ID: import.meta.env.VITE_APP_CLIENT_ID,
+                        redirect_sso: 1//import.meta.env.VITE_APP_REDIRECT_SSO_URL
+                    });
+                
+                    const authResult = await curdapi2.getAuth();
+                    await store.dispatch(`auth/${AUTH_TOKEN}`, { ...authResult, thirdParty: curdapi2 });
+                    const userProfile = await store.dispatch(`auth/${AUTH_GET_USER}`);
+                    await store.dispatch(`auth/${AUTH_USER}`, userProfile)
+
+                } catch (e) {
+                    console.error(e);
+                }
+            });
+        }
     },
     mounted() {
         setTimeout(() => {
             this.loading = false
-        }, 5000)
+        }, 5000);
+
+        this.$nextTick(async() => {
+            console.log("init dapi2");
+            await this.initDapi2();
+        })
     },
     created() {
 
