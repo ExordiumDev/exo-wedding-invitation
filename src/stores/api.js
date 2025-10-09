d6512aa33ea6527a917ffee4a0a6e19747075d22
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-09 18:03:03 +0700

    [add] req-headers

--- src/stores/api.js
+++ src/stores/api.js
@@ -1,7 +1,6 @@
 import store from '../stores';
 import axios from 'axios';
 import router from '../router/index';
-import { isShallow } from 'vue';
 
 const $axios = axios.create({
     withCredentials: true,
@@ -9,77 +8,26 @@ const $axios = axios.create({
     headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-    }
-});
-
-const $axInstance = axios.create({
-    // baseURL: '/api',
-    headers:{
-        'X-Client-Id': import.meta.env.VITE_APP_CLIENT_ID,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
     }
 });
 
 let akses = undefined;
-let usrLogout = false
+$axios.interceptors.request.use(async (config) => {
+    // Authorization Header
+    if (akses?.access_token) {
+        config.headers['Authorization'] = `${akses.token_type} ${akses.access_token}`;
+    }
 
-function setupInterceptor(instance) {
-    instance.interceptors.request.use(
-        async function (config) {
-            config.headers['Authorization'] = `${akses?.token_type} ${akses?.access_token}`;
-            return config;
-        },
-        function (error) {
-            return Promise.reject(error);
-        }
-    );
-}
+    // CSRF Header
+    const csrfToken = getCookie("XSRF-TOKEN");
+    if (csrfToken) {
+        config.headers["X-CSRF-TOKEN"] = csrfToken;
+    } else {
+        console.log("XSRF-TOKEN not found in cookies");
+    }
 
-setupInterceptor($axios);
-
-// $axios.interceptors.request.use (   
-//     function (config) {
-//         config.headers['Authorization'] = `${akses?.token_type} ${akses?.access_token}`; 
-//         return config;
-//     },
-//     async function (error) {
-//         NProgress.done();
-//         const originalRequest = error.config;
-//         if(error.code != "ERR_NETWORK" && error.response){
-//             const errMessage = {status:error.response.status}
-//             if (error.response.status === 401 && !originalRequest._retry) {
-//                 try {
-//                     if (!curdapi2) { throw new Error(`curdapi2 : ${curdapi2}`); }
-//                     const vResult = await curdapi2.getRefreshToken(akses)
-//                     const {access_token,expires_in,refresh_token,token_type} = vResult;
-//                     akses = {access_token,expires_in,refresh_token,token_type};
-//                     $axInstance.defaults.headers['Authorization'] = `${token_type} ${access_token}`;
-//                     originalRequest._retry = true;
-//                     return $axInstance(originalRequest);
-//                 } catch (error) {
-//                     errMessage.message ='unauthentication'
-//                 }
-//             }else{
-//                 switch (error.response.status) {
-//                     case 403:
-//                         errMessage.message ='not found' 
-//                         break;
-//                     case 500:
-//                         errMessage.message ='internal server error';
-//                         break;
-//                     default:
-//                         errMessage.message=error.response?.data;
-//                         break;
-//                 }
-//             }
-//             throw errMessage
-//         }
-//         return Promise.reject(error);
-//     }
-// );
+    return config;
+}, (error) => Promise.reject(error));
 
 $axios.interceptors.response.use(
     (response) => response,
@@ -96,7 +44,6 @@ $axios.interceptors.response.use(
             try {
                 await $axios.post(import.meta.env.VITE_APP_API_URL+'/auth/refresh', {}, { withCredentials: true });
                 // const newData = res.data;
-                // setAuthToken(newData);
                 await store.dispatch('auth/AUTH_GET_USER');
                 return $axios(originalRequest);
             } catch (err) {
@@ -128,25 +75,10 @@ export const setCookie = (name, value, days)=>{
     }
 }
 
-export const getCookie = (name=undefined) =>{
-    const curvalue = document.cookie || ''
-    const value = `; ${curvalue}`;
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
-export const delCookie = (name=undefined) =>{
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-}
-
-export const setAuthToken = (payload) => {
-    if (payload) {
-        const {access_token,refresh_token,expires_in,token_type} = payload;
-        akses = {access_token,refresh_token,expires_in,token_type};
-        $axios.defaults.headers['Authorization'] = `${token_type} ${access_token}`;
-    } else {
-        delete $axios.defaults.headers['Authorization'];
-    }
-};
-
-export { $axInstance, $axios };
+export { $axios };
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,9 +1,5 @@
-import { setAuthToken,setCookie,getCookie,delCookie } from './api';
-import {$axInstance, $axios} from './api.js';
-import CryptoJS from 'crypto-js';
+import {$axios} from './api.js';
 import { SET_USER_IF_UNAUTHENTICATED, EX_CODE, GOOGLE_LOGOUT, CHECK_AUTH, SET_USER, AUTH_GET_GOOGLE_TOKEN, AUTH_TOKEN,AUTH_USER,SOCKET_CLIENT,AUTH_GET_USER,AUTH_PROFILE,AUTH_STATUS,AUTH_LOGOUT,AUTH_DESTROY_SESSION,AUTHENTICATOR,LOGOUT,DELETEALL_COOKIES } from './actions/reqApi.js';
-const APP_JWT_SECRET = import.meta.env.VITE_APP_JWT_SECRET;
-import 'url-search-params-polyfill';
 
 const state = {
     SET_USER: null,
@@ -33,7 +29,6 @@ const mutations = {
         state.AUTH_TOKEN = resp;
     },
     [AUTH_USER](state, resp){
-        console.log('pake mutasi ', resp)
         state.AUTH_USER = resp;
     },
     [SOCKET_CLIENT](state, resp){
@@ -67,7 +62,7 @@ const actions = {
     [GOOGLE_LOGOUT]: async ({ commit }) => {
         try {
             const res = await $axios.post("/auth/logout");
-            commit('SET_USER', null);
+            commit(AUTH_USER, {});
             return res
         } catch (error) {
             console.error("Not authenticated", error);
@@ -77,7 +72,6 @@ const actions = {
     [CHECK_AUTH]: async ({ commit }) => {
         try {
             const res = await $axios.get("/auth/m");
-            console.log('res dari check auth', res)
             commit(SET_USER, res.data);
         } catch (error) {
             commit(SET_USER, null);
@@ -99,10 +93,8 @@ const actions = {
 
     [AUTH_USER]: async ({ commit }, payload) => {
         try {
-            console.log('ini jalan ga ?')
             if (!(payload instanceof Object) || !payload.user) throw new Error('invalid user');
             commit('SET_USER', payload.user);
-            console.log('User setted in Vuex:', payload.user);
         } catch (error) {
             commit('SET_USER', null);
             console.error(error);
@@ -113,7 +105,6 @@ const actions = {
         try {
             if(!(payload instanceof Object)) throw 'invalid token';
             commit(AUTH_TOKEN,payload);
-            setAuthToken(payload);
             commit(AUTH_STATUS,true);
         } catch (error) {
             commit(AUTH_STATUS,false);
@@ -124,7 +115,7 @@ const actions = {
 
     [AUTH_GET_USER]({ commit, dispatch, rootState }, payload) {
         return new Promise((resolve, reject) => {
-            console.log('pake auth get user')
+            
             $axios.get("/auth/m").then(async(response) => {
                 resolve(response?.data)
                 commit(AUTH_USER, response?.data)
@@ -132,196 +123,6 @@ const actions = {
                 reject(error)
             });
         })
-    },
-
-
-    [AUTH_DESTROY_SESSION]:({commit, dispatch},param)=>{
-        return new Promise((resolve,reject)=>{
-        $axInstance({ url: `${import.meta.VITE_APP_OAUTH_URL}/api/user/sessions/`+param, method: "DELETE"}).then(resp => {
-                resolve(resp.data);
-            }).catch(err => {
-                reject(err.response);
-            });
-        });
-    },
-    [AUTHENTICATOR]:({commit, dispatch, rootGetters, rootState}, payload)=>{
-        let urlparam = new URLSearchParams({
-            response_type: 'code',
-            scope: '',
-            client_id: import.meta.env.VITE_APP_CLIENT_ID,
-            redirect_uri: import.meta.env.VITE_APP_CLIENT_REDIRECT,
-            state:sessionStorage.getItem('dapi')
-        }).toString();
-        return new Promise((resolve, reject) => {
-            $axInstance.get(`${import.meta.VITE_APP_OAUTH_URL}/oauth/authorize?${urlparam}`, {
-                // withCredentials: true,
-                headers: {
-                    'Cache-Control': 'no-cache',
-                    'Content-Type': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            }).then(async(response) => {
-                resolve(true);
-            }).catch(async (error) => {
-                // console.log({testauth:error.respo'nse.headers["set-cookie"]})
-                // console.log(document.cookie);'
-                reject(error?.response?.data)
-            });
-        })
-    },
-    [LOGOUT]:({commit, dispatch, rootGetters, rootState}, payload)=>{
-        // let urlparam = new URLSearchParams({
-        //     client_id: import.meta.MIX_CLIENT_ID,
-        //     redirect_uri: import.meta.MIX_CLIENT_REDIRECT,
-        //     response_type: 'code',
-        //     scope: 'userProfile'
-        // }).toString();
-        return new Promise((resolve, reject) => {
-            $axInstance.post(`${import.meta.VITE_APP_OAUTH_URL}/api/user/sessions/`+rootGetters['auth/'+SOCKET_CLIENT]?.id).then(async(response) => {
-                // await dispatch(DELETEALL_COOKIES);
-                await $cookies.remove('dapi');
-                commit(AUTH_STATUS,false);
-                commit(AUTH_TOKEN,{});
-                commit(AUTH_USER,{});
-                window.location.href = `${import.meta.VITE_APP_OAUTH_URL}/login`;
-                await resolve(true);
-            }).catch(async (error) => {
-                // await dispatch(DELETEALL_COOKIES);
-                await $cookies.remove('dapi');
-                commit(AUTH_STATUS,false);
-                commit(AUTH_TOKEN,{});
-                commit(AUTH_USER,{});
-                await $('div.jala-signIn2').next('iframe').addClass('py-1').css({'display': 'block','width': '85px','border': '0px','height': '40px','margin': '0px','z-index': '1'}).appendTo('#btn-login');
-                window.location.href = `${import.meta.VITE_APP_OAUTH_URL}/login`;
-                reject(error?.response?.data)
-            });
-        })
-    },
-    [DELETEALL_COOKIES]:({commit, dispatch, getters, rootState}, payload)=>{
-        const cookieNames = $cookies.keys();
-        // Iterate through all cookie names and delete them
-        for (const cookieName of cookieNames) {
-            $cookies.remove(cookieName);
-        }
-    },
-    login({ commit }, payload) {
-        localStorage.setItem('token', null) //RESET LOCAL STORAGE MENJADI NULL
-        commit('SET_TOKEN', null, { root: true }) //RESET STATE TOKEN MENJADI NULL
-        $cookies.remove('cors_account');
-        commit('ASSIGN_USER_AUTH', null, { root: true })
-        return new Promise((resolve, reject) => {
-            payload.append('API_KEY',import.meta.VITE_APP_API_KEY);
-            $axInstance.post(`${import.meta.VITE_APP_OAUTH_URL}/api/auth/login`, payload)
-            .then((response) => {
-                if (response.data.success) {
-                    var data = response.data.data;
-                    window.location.href = vm.baseURL();
-                    // commit('SET_TOKEN', data.api_token, { root: true })
-                    CAD.setData('scope',data);
-                    localStorage.setItem('token', data.api_token);
-                    $cookies.set('cors_account',CryptoJS.AES.encrypt(JSON.stringify(data), APP_JWT_SECRET).toString());
-                    commit('ASSIGN_USER_AUTH', data, { root: true });
-                    localStorage.removeItem('DataTables_requestdataTable');
-                    localStorage.removeItem('DataTables_pprdataTable');
-                    localStorage.removeItem('DataTables_pbsdataTable');
-                    // vm.$router.push({ name: 'dashboard' }).catch(()=>{});
-                }
-                //JANGAN LUPA UNTUK MELAKUKAN RESOLVE AGAR DIANGGAP SELESAI
-                resolve(response.data)
-            })
-            .catch((error) => {
-                if (error.response.status == 422) {
-                    commit('SET_ERRORS', error.response.data, { root: true })
-                }
-                reject(error)
-            })
-        })
-    },
-    getToken({ commit, dispatch, rootState }, payload) {
-        return new Promise((resolve, reject) => {
-            // const formData = new FormData();
-            // formData.append('grant_type','password');
-            // formData.append('client_id',`${import.meta.MIX_CLIENT_ID}`);
-            // formData.append('client_secret',`${import.meta.MIX_CLIENT_SECRET}`);
-            // formData.append('username',rootState.authenticated?.email);
-            // formData.append('password','password');
-            $axInstance.get(`${import.meta.env.VITE_APP_OAUTH_URL}/api/oauth2/gettoken`).then(async (response) => {
-                if(response?.data?.access_token){
-                    await commit('SET_TOKEN', response?.data?.access_token, { root: true })
-                    const expiresInSeconds = 604800;
-                    await $cookies.set('jala_acctoken',CryptoJS.AES.encrypt(JSON.stringify(response?.data?.access_token), APP_JWT_SECRET).toString(), { expires: expiresInSeconds });
-                }
-                resolve(response?.data)
-            }).catch((error) => {
-                reject(error?.response?.data)
-            });
-        })
-    },
-    getTokenID({ commit, dispatch, rootState }, payload) {
-        return new Promise((resolve, reject) => {
-            $axInstance.get(`${import.meta.env.VITE_APP_OAUTH_URL}/api/oauth2/token_id`, {
-                withCredentials: true,
-                headers: {
-                    'Cache-Control': 'no-cache',
-                    'Content-Type': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            }).then(async(response) => {
-                if(response?.data?.token){
-                    await commit('SET_TOKEN_ID', response?.data?.token, { root: true })
-                    const expiresInSeconds = 604800;
-                    await $cookies.set('jala_tokenid',CryptoJS.AES.encrypt(JSON.stringify(response?.data?.token), APP_JWT_SECRET).toString(), { expires: expiresInSeconds });
-                }
-                resolve(response?.data)
-            }).catch((error) => {
-                reject(error?.response?.data)
-            });
-        })
-    },
-    
-    logout({ commit, dispatch, rootState }, payload){
-        let urlparam = new URLSearchParams({
-            client_id: import.meta.VITE_APP_CLIENT_ID,
-            redirect_uri: import.meta.VITE_APP_REDIRECT_SSO_URL,
-            response_type: 'code',
-            scope: 'userProfile'
-        }).toString();
-        return new Promise((resolve, reject) => {
-            $axInstance.post(`${import.meta.VITE_APP_OAUTH_URL}/api/oauth2/logout`).then(async(response) => {
-                await dispatch('deleteAllCookies');
-                await commit('ASSIGN_USER_AUTH', null, { root: true });
-                await commit('SET_TOKEN', null, { root: true });
-                window.location.href = `${import.meta.VITE_APP_OAUTH_URL}/api/oauth/authorize?`+urlparam;
-                await resolve(true);
-            }).catch(async (error) => {
-                if(error?.response?.status==401){
-                    await dispatch('deleteAllCookies');
-                    await commit('ASSIGN_USER_AUTH', null, { root: true });
-                    await commit('SET_TOKEN', null, { root: true });
-                    await $('div.jala-signIn2').next('iframe').addClass('py-1').css({'display': 'block','width': '85px','border': '0px','height': '40px','margin': '0px','z-index': '1'}).appendTo('#btn-login');
-                    window.location.href = `${import.meta.VITE_APP_OAUTH_URL}/api/oauth/authorize?`+urlparam;
-                }
-                reject(error?.response?.data)
-            });
-        })
-    },
-    deleteAllCookies({ commit, dispatch, rootState }, payload) {
-        const cookieNames = $cookies.keys();
-        // Iterate through all cookie names and delete them
-        for (const cookieName of cookieNames) {
-            $cookies.remove(cookieName);
-        }
-    },
-    validation({ commit }, payload){
-        var errors = {};
-        $.each(payload,function(el, value){
-            if(!value){
-                errors[el] = el +' is required';
-            }
-        });
-        var result = (Object.values(errors).length>0) ? false:true;
-        commit('SET_RESULT', result, { root: true })
-        commit('SET_ERRORS', errors, { root: true });
     },
 };
 
