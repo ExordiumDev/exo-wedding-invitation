5f0432ad3bdcb604bbb56f0f37b4c2e73d7fd88e
Widodo <widodo.rahadi00@gmail.com>
2025-10-13 23:49:55 +0700

    [add] global snackbar config

--- src/appcore/globalTest.js
+++ src/appcore/globalTest.js
@@ -15,6 +15,12 @@ export default {
         },
         pushRoute(route) {
             this.$router.push(route)
+        },
+        showSnackbar(state, message, color, timeout) {
+            this.$store.commit('SET_SB_TO', timeout || 2000);
+            this.$store.commit('SET_SB_TEXT', message);
+            this.$store.commit('SET_SB_COLOR', color);
+            this.$store.commit('SET_SB_SHOW', state);
         }
     }
 }
