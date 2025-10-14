142df65dd74207d78d6eb52e0c4caa0234aa6279
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-14 18:10:08 +0700

    [update] save loc stor

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -39,13 +39,16 @@
                         </v-window-item>
 
                         <v-window-item value="signup">
-                            <v-card-text class="pa-7">
-                                <v-text-field variant="outlined" label="Name" required />
-                                <v-text-field variant="outlined" label="E-mail" required />
-                                <v-text-field variant="outlined" label="Password" type="password" required />
-                                <v-text-field variant="outlined" label="Confirm Password" type="password" required />
-                                <v-btn block color="exr_accent_orange_600" class="mt-4">Register</v-btn>
+                            <v-card-text class="pa-5">
+                                <v-text-field variant="outlined" label="Name" required v-model="uSignupN" />
+                                <v-text-field variant="outlined" label="E-mail" required v-model="uSignupE" />
+                                <v-text-field variant="outlined" label="Password" type="password" required v-model="uSignupP" ref="uSignupP" @input="checkMatch"/>
+                                <v-text-field variant="outlined" label="Confirm Password" type="password" required ref="uSignupCP" @input="checkMatch"/>
+                                <small class="text-red" v-if="errorField">{{ errorField }}</small>
                             </v-card-text>
+                            <v-card-actions>
+                                <v-btn variant="flat" block color="exr_accent_orange_600" @click="signup">Register</v-btn>
+                            </v-card-actions>
                         </v-window-item>
                     </v-window>
                 </v-card>
@@ -57,7 +60,7 @@
 <script>
 
 import { mapActions } from 'vuex';
-import { AUTH_SIGNIN } from '../stores/actions/reqApi'
+import { AUTH_SIGNIN, AUTH_SIGNUP } from '../stores/actions/reqApi'
 import { encPass } from '../appcore/crypto'
 
 export default { 
@@ -69,6 +72,11 @@ export default {
             eloginResp: false,
             sloginResp: false,
             loginRespTxt: '',
+
+            uSignupN: '',
+            uSignupE: '',
+            uSignupP: '',
+            errorField: '',
         }
     },
     computed: {
@@ -78,8 +86,47 @@ export default {
     },
     methods: {
         ...mapActions({
-            actAUTH_SIGNIN: `auth/${AUTH_SIGNIN}`
+            actAUTH_SIGNIN: `auth/${AUTH_SIGNIN}`,
+            actAUTH_SIGNUP: `auth/${AUTH_SIGNUP}`
         }),
+        checkMatch() {
+            const _p = this.$refs.uSignupP.$el.querySelector('input').value;
+            const _cp = this.$refs.uSignupCP.$el.querySelector('input').value;
+
+            if(_p.length === 0 && _cp.length === 0) {
+                this.errorField = '';
+                return
+            }
+
+            if (_p.length < 8) {
+                this.errorField = 'Password must be at least 8 characters.'
+            } else if (_p !== _cp) {
+                this.errorField = 'Confirm Password is not correct yet!'
+            } else {
+                this.errorField = ''
+            }
+        },
+        async signup() {
+            console.log('sign up');
+            const encPas = encPass(this.uSignupP)
+            const payload = {
+                name: this.uSignupN,
+                email: this.uSignupE,
+                password: encPas
+            }
+            try {
+                this.$store.commit('SET_PROGRESS_BAR', true)
+                const res = await this.actAUTH_SIGNUP(payload);
+                this.showSnackbar(true, res?.response?.data?.detail || 'signup succeed.', 'success', 2500);
+                localStorage.setItem('v_em', this.uSignupE);
+                this.$router.push('/v');
+            } catch (error) {
+                console.log('error ', error);
+                this.showSnackbar(true, error?.response?.data?.detail || 'An error occurred during sign up.', 'error', 2500);
+            } finally {
+                this.$store.commit('SET_PROGRESS_BAR', false)
+            }
+        },
         async SignIn() {
             try {
                 const e = encPass(this.uP);
