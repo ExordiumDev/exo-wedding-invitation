478d81fc46911a781f0bb4c5b3355e3ad52f09bf
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-14 14:50:29 +0700

    [add] google auth loading screen

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,15 +1,11 @@
 <template>
     <v-app>
-        <Header v-if="isRender" :authUser="getAUTH_USER" />
-        <v-snackbar v-model="$store.state.sbShow" :timeout="$store.state.sbTimeout" location="top" :color="$store.state.sbColor" class="elevation-24">{{ $store.state.sbText }}</v-snackbar>
-        <v-dialog max-width="550" persistent v-model="$store.state.authDialog">
-            <signDialog @close="$store.commit('SET_AUTH_DIALOG', false)" />
-        </v-dialog>
+        <v-snackbar v-model="$store.state.sbShow" :timeout="$store.state.sbTimeout" location="top" :color="$store.state.sbColor">{{ $store.state.sbText }}</v-snackbar>
         <v-progress-linear v-if="$store.state.pageLoad" color="primary" height="5" indeterminate absolute top></v-progress-linear>
         <v-main class="main-wrapper">
-            <router-view></router-view>
+            <loadingAuth v-if="$store.state.auth.isAuthenticating" />
+            <router-view />
         </v-main>
-        <!-- <Footer /> -->
     </v-app>
 </template>
 
@@ -18,14 +14,15 @@
 </style>
 
 <script>
+import { reloadAuthState } from '../main.js'
+import loadingAuth from '../components/LoadingAuth.vue'
 import signDialog from '../components/signDialog.vue'
-import { mapActions, mapGetters, mapMutations } from 'vuex';
+import { mapState, mapActions, mapGetters, mapMutations } from 'vuex';
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import BaseIndex from './BaseIndex.vue';
 import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 import { AUTH_TOKEN, AUTH_USER, AUTH_GET_USER, EX_CODE, SET_USER_IF_UNAUTHENTICATED } from '../stores/actions/reqApi.js'
-import { mapState } from 'vuex/dist/vuex.cjs.js';
 
 export default {
     name: 'App',
@@ -42,6 +39,7 @@ export default {
         Footer,
         DotLottieVue,
         signDialog,
+        loadingAuth,
     },
     computed: {
         ...mapGetters({
@@ -59,28 +57,29 @@ export default {
             actAUTH_USER: `auth/${AUTH_USER}`,
         }),
         ...mapMutations({
-            mutSET_USER_IF_UNAUTHENTICATED: `auth/${SET_USER_IF_UNAUTHENTICATED}`
+            mutSET_USER_IF_UNAUTHENTICATED: `auth/${SET_USER_IF_UNAUTHENTICATED}`,
+            mutSET_LOADING_AUTH: 'auth/SET_AUTHENTICATING'
         }),
     },
     mounted() {
         this.$nextTick(async() => {
-            try {
-                const urlParams = new URLSearchParams(window.location.search);
-                const code = urlParams.get("code");
-                
-                if(code){
+            // const urlParams = new URLSearchParams(window.location.search);
+            // const code = urlParams.get("code");
+            const code = new URLSearchParams(window.location.search).get('code')
+            if(code){
+                this.mutSET_LOADING_AUTH(true);
+                try {
                     await this.actEX_CODE(code);
-                    await this.actAUTH_GET_USER();
-                    await this.$router.push("/")
-                    location.reload();
-                } else { 
-                    await this.actAUTH_GET_USER();
+                    window.history.replaceState({}, document.title, window.location.pathname)
+                    await reloadAuthState()
+                    // window.location.reload();
+                    
+                } catch (error) {
+                    console.error('Google login error ', error);
+                } finally {
+                    this.mutSET_LOADING_AUTH(false)
                 }
-            } catch (error) {
-                console.error(error);
-            } finally {
-                this.isRender = true;
-            }
+            } 
         })
     },
 }
