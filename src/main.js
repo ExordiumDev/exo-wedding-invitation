be98a5d627e1296e2b8f2c59c4ebb9adefaf7d57
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-14 14:48:21 +0700

    [add] check auth func

--- src/main.js
+++ src/main.js
@@ -15,11 +15,25 @@ import '@fontsource/dm-sans/400.css';
 import '@fontsource/dm-sans/700.css';
 import LoadScript  from "vue-plugin-load-script"
 
-const app = createApp(App);
-app.use(VueCookies);
-app.use(global);
-app.mixin(globalTest);
-app.component('AppAlert', AppAlert );
-app.use(LoadScript );
+async function initApp() {
+    try {
+        await store.dispatch('auth/AUTH_GET_USER');
+    } catch (error) {
+        console.error('unauthenticated', error)
+    }    
+    const app = createApp(App);
+    app.use(VueCookies);
+    app.use(global);
+    app.mixin(globalTest);
+    app.component('AppAlert', AppAlert );
+    app.use(LoadScript );
 
-app.use(router).use(store).use(vuetify).mount("#app");
+    app.use(router).use(store).use(vuetify).mount("#app");
+}
+
+initApp()
+export async function reloadAuthState() {
+    await store.dispatch('auth/AUTH_GET_USER').then(() => {
+        router.push('/dashboard')
+    });
+}
